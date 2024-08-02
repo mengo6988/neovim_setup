e4f7d3bba772c188a346df89d9b516a3b1ad8040
mengo <menghong6988@gmail.com>
2024-08-02 12:39:43 +0800

    changed lsp

--- lua/mengo/plugins/lsp.lua
+++ lua/mengo/plugins/lsp.lua
@@ -179,13 +179,13 @@ return {
 				loadfile(ft_path)()
 			end
 
-			vim.keymap.set({ "i", "s" }, "<c-k>", function()
+			vim.keymap.set({ "i", "s" }, "<c-j>", function()
 				if ls.expand_or_jumpable() then
 					ls.expand_or_jump()
 				end
 			end, { silent = true })
 
-			vim.keymap.set({ "i", "s" }, "<c-j>", function()
+			vim.keymap.set({ "i", "s" }, "<c-k>", function()
 				if ls.jumpable(-1) then
 					ls.jump(-1)
 				end
