83dcb0e3161616402c7e353ad3870561a447a6c6
mengo <menghong6988@gmail.com>
2024-08-28 22:53:09 +0800

    added ft_plugin

--- lua/mengo/plugins/init.lua
+++ lua/mengo/plugins/init.lua
@@ -30,8 +30,14 @@ return {
 		config = function()
 			require("conform").setup({
 				formatters_by_ft = {
-					python = { "black", "isort" },
 					lua = { "stylua" },
+					python = function(bufnr)
+						if require("conform").get_formatter_info("ruff_format", bufnr).available then
+							return { "ruff_format" }
+						else
+							return { "isort", "black" }
+						end
+					end,
 					javascript = { "prettier" },
 					typescript = { "prettier" },
 					rust = { "rustfmt" },
