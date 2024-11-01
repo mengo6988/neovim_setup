b92b8bf72f531828688d17871c0ee40da5f329cf
Lewis lee <minecraftpe132006@gmail.com>
2024-11-01 13:50:46 +0800

    Added more filepath info for lualine

--- lua/mengo/plugins/init.lua
+++ lua/mengo/plugins/init.lua
@@ -123,7 +123,7 @@ return {
 				sections = {
 					lualine_a = { 'mode' },
 					lualine_b = { 'branch', 'diff', 'diagnostics' },
-					lualine_c = { 'filename' },
+					lualine_c = { {'filename', file_status=true, path=1 }},
 					lualine_x = { 'encoding', 'fileformat', 'filetype' },
 					lualine_y = { 'searchcount', 'selectioncount', 'progress' },
 					lualine_z = { 'location' }
