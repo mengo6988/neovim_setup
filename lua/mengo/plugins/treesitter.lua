5bc9c3af6f58bcdfa3b5521f73bc3ab1b1008c10
mengo <menghong6988@gmail.com>
2024-11-02 11:30:48 +0800

    Added nu shell support

--- lua/mengo/plugins/treesitter.lua
+++ lua/mengo/plugins/treesitter.lua
@@ -1,124 +1,130 @@
 return {
-	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate',
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-context",
-			"nvim-treesitter/nvim-treesitter-textobjects"
-		},
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-context",
+      "nvim-treesitter/nvim-treesitter-textobjects"
+    },
 
-		config = function()
-			require('nvim-treesitter.configs').setup({
-				-- A list of parser names, or "all" (the five listed parsers should always be installed)
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "javascript", "typescript", "bash", "go", "xml", "http", "json", "graphql", "java" },
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        -- A list of parser names, or "all" (the five listed parsers should always be installed)
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "javascript", "typescript", "bash", "go", "xml", "http", "json", "graphql", "java" },
 
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = false,
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
 
-				-- Automatically install missing parsers when entering buffer
-				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-				auto_install = true,
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = true,
 
-				-- List of parsers to ignore installing (or "all")
-				ignore_install = { "javascript" },
+        -- List of parsers to ignore installing (or "all")
+        ignore_install = { "javascript" },
 
-				---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-				-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-				indent = {
-					enable = true
-				},
+        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+        indent = {
+          enable = true
+        },
 
-				highlight = {
-					enable = true,
+        highlight = {
+          enable = true,
 
-					-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-					-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-					-- the name of the parser)
-					-- list of language that will be disabled
-					-- disable = { "c", "rust" },
-					-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-					disable = function(lang, buf)
-						local max_filesize = 100 * 1024 -- 100 KB
-						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-						if ok and stats and stats.size > max_filesize then
-							return true
-						end
-					end,
+          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+          -- the name of the parser)
+          -- list of language that will be disabled
+          -- disable = { "c", "rust" },
+          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
 
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = false,
-				},
-			})
-		end
-	},
-	{
-		'nvim-treesitter/nvim-treesitter-context',
-		config = function()
-			require 'treesitter-context'.setup {
-				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-				line_numbers = true,
-				multiline_threshold = 20, -- Maximum number of lines to show for a single context
-				trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-				mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
-				-- Separator between context and content. Should be a single character string, like '-'.
-				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-				separator = nil,
-				zindex = 20, -- The Z-index of the context window
-				on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-			}
-		end
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				textobjects = {
-					select = {
-						enable = true,
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end,
 
-						-- Automatically jump forward to textobj, similar to targets.vim
-						lookahead = true,
+    dependencies = {
+      -- NOTE: additional parser
+      { "nushell/tree-sitter-nu", build = ":TSUpdate nu" },
+    },
+    build = ":TSUpdate",
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require 'treesitter-context'.setup {
+        enable = true,        -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0,        -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = 'cursor',      -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      }
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          select = {
+            enable = true,
 
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
-							["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-							["=l"] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-							["=r"] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
 
-							["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
-							["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+              ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+              ["=l"] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+              ["=r"] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
 
-							["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
-							["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+              ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+              ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
 
-							["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
-							["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+              ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+              ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
 
-							["af"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
-							["if"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+              ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+              ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
 
-							["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
-							["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
-						},
-					},
-					swap = {
-						enable = true,
-						swap_next = {
-							["<leader>na"] = "@parameter.inner", -- swap parameters/argument with next
-						},
-						swap_previous = {
-							["<leader>pa"] = "@parameter.inner", -- swap parameters/argument with prev
-						},
-					},
+              ["af"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+              ["if"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
 
-				}
-			})
-		end
-	}
+              ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>na"] = "@parameter.inner", -- swap parameters/argument with next
+            },
+            swap_previous = {
+              ["<leader>pa"] = "@parameter.inner", -- swap parameters/argument with prev
+            },
+          },
+
+        }
+      })
+    end
+  },
 }
