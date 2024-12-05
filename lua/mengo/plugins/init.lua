b52627fe2071c476e0ae57b683070610db010b81
mengo <menghong6988@gmail.com>
2024-12-06 01:16:49 +0800

    Feat: remove stupid autosave

--- lua/mengo/plugins/init.lua
+++ lua/mengo/plugins/init.lua
@@ -300,7 +300,6 @@ return {
   {
     'brianhuster/live-preview.nvim',
     dependencies = {
-      'brianhuster/autosave.nvim',     -- Not required, but recomended for autosaving and sync scrolling
 
       -- You can choose one of the following pickers
       'nvim-telescope/telescope.nvim',
