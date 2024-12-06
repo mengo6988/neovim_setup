8819523015b92107a366bccf7060341173af1253
mengo <menghong6988@gmail.com>
2024-12-07 00:44:14 +0800

    Feat: Added support for golang

--- lua/mengo/plugins/lsp.lua
+++ lua/mengo/plugins/lsp.lua
@@ -1,209 +1,228 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-cmdline',
-			'hrsh7th/nvim-cmp',
-			'L3MON4D3/LuaSnip',
-			'saadparwaiz1/cmp_luasnip',
-			'j-hui/fidget.nvim',
-		},
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/nvim-cmp',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'j-hui/fidget.nvim',
+    },
 
-		config = function()
-			local cmp_lsp = require("cmp_nvim_lsp")
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities(),
-				cmp_lsp.default_capabilities())
+    config = function()
+      local cmp_lsp = require("cmp_nvim_lsp")
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        cmp_lsp.default_capabilities())
 
-			require("fidget").setup({})
-			require("mason").setup()
-			require('java').setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"rust_analyzer",
-					"clangd",
-					"ts_ls",
-					"pyright",
-					"solidity_ls_nomicfoundation",
-				},
-				handlers = {
-					function(server_name) -- default handler (optional)
-						require("lspconfig")[server_name].setup {
-							capabilities = capabilities
-						}
-					end,
-					-- Settings for lua to set vim as global
-					["lua_ls"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.lua_ls.setup {
-							capabilities = capabilities,
-							settings = {
-								Lua = {
-									diagnostics = {
-										globals = { "vim" }
-									}
-								}
-							}
-						}
-					end,
-				}
-			})
-			local kind_icons = {
-				Text = "󰉿",
-				Method = "󰆧",
-				Function = "󰊕",
-				Constructor = "",
-				Field = " ",
-				Variable = "󰀫",
-				Class = "󰠱",
-				Interface = "",
-				Module = "",
-				Property = "󰜢",
-				Unit = "󰑭",
-				Value = "󰎠",
-				Enum = "",
-				Keyword = "󰌋",
-				Snippet = "",
-				Color = "󰏘",
-				File = "󰈙",
-				Reference = "",
-				Folder = "󰉋",
-				EnumMember = "",
-				Constant = "󰏿",
-				Struct = "",
-				Event = "",
-				Operator = "󰆕",
-				TypeParameter = " ",
-				Misc = " ",
-			}
+      require("fidget").setup({})
+      require("mason").setup()
+      require('java').setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "rust_analyzer",
+          "clangd",
+          "ts_ls",
+          "pyright",
+          "solidity_ls_nomicfoundation",
+          "gopls",
+        },
+        handlers = {
+          function(server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {
+              capabilities = capabilities
+            }
+          end,
+          -- Settings for lua to set vim as global
+          ["lua_ls"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup {
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { "vim" }
+                  }
+                }
+              }
+            }
+          end,
+          ["gopls"] = function()
+            local lspconfig = require("lspconfig")
+            local util = require "lspconfig/util"
+            lspconfig.gopls.setup {
+              capabilities = capabilities,
+              cmd = { "gopls" },
+              filetypes = { "go", "gomod", "gowork", "gotmpl" },
+              root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+              settings = {
+                gopls = {
+                  completeUnimported = true,
+                  usePlaceholders = true,
+                  analyses = {
+                    unusedparams = true,
+                  },
+                },
+              }, }
+          end,
+        }
+      })
+      local kind_icons = {
+        Text = "󰉿",
+        Method = "󰆧",
+        Function = "󰊕",
+        Constructor = "",
+        Field = " ",
+        Variable = "󰀫",
+        Class = "󰠱",
+        Interface = "",
+        Module = "",
+        Property = "󰜢",
+        Unit = "󰑭",
+        Value = "󰎠",
+        Enum = "",
+        Keyword = "󰌋",
+        Snippet = "",
+        Color = "󰏘",
+        File = "󰈙",
+        Reference = "",
+        Folder = "󰉋",
+        EnumMember = "",
+        Constant = "󰏿",
+        Struct = "",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = " ",
+        Misc = " ",
+      }
 
-			local lsp = vim.lsp
-			lsp.handlers["textDocument/hover"] = lsp.with(vim.lsp.handlers.hover, {
-				border = "rounded",
-			})
+      local lsp = vim.lsp
+      lsp.handlers["textDocument/hover"] = lsp.with(vim.lsp.handlers.hover, {
+        border = "rounded",
+      })
 
-			local cmp = require('cmp')
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
-			local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-			cmp.setup({
-				snippet = {
-					-- REQUIRED -must specify a snipper engine
-					expand = function(args)
-						require('luasnip').lsp_expand(args.body)
-					end,
-				},
-				window = {
-					-- completion = cmp.config.window.bordered(),
-					documentation = {
-						border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-					},
-				},
-				mapping = {
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-u>'] = cmp.mapping.scroll_docs(4),
-					['<C-i>'] = cmp.mapping.scroll_docs(-4),
-					['<C-e>'] = cmp.mapping.abort(),
-					["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-					["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-					["<C-y>"] = cmp.mapping(
-						cmp.mapping.confirm {
-							behavior = cmp.ConfirmBehavior.Insert,
-							select = true,
-						},
-						{ "i", "c" }
-					),
-				},
+      local cmp = require('cmp')
+      local cmp_select = { behavior = cmp.SelectBehavior.Select }
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      cmp.setup({
+        snippet = {
+          -- REQUIRED -must specify a snipper engine
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          documentation = {
+            border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+          },
+        },
+        mapping = {
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-u>'] = cmp.mapping.scroll_docs(4),
+          ['<C-i>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+          ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+          ["<C-y>"] = cmp.mapping(
+            cmp.mapping.confirm {
+              behavior = cmp.ConfirmBehavior.Insert,
+              select = true,
+            },
+            { "i", "c" }
+          ),
+        },
 
-				-- ['<Tab>'] = cmp.mapping(function(fallback)
-				-- 	if cmp.visible() then
-				-- 		cmp.select_next_item()
-				-- 	else
-				-- 		fallback()
-				-- 	end
-				-- end, { "i", "s", }),
-				-- ['<S-Tab>'] = cmp.mapping(function(fallback)
-				-- 	if cmp.visible() then
-				-- 		cmp.select_prev_item()
-				-- 	else
-				-- 		fallback()
-				-- 	end
-				-- end, { "i", "s", }),
-				formatting = {
-					fields = { "kind", "abbr", "menu" },
-					format = function(entry, vim_item)
-						-- Kind icons
-						vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-						-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-						vim_item.menu = ({
-							nvim_lsp = "[LSP]",
-							luasnip = "[Snippet]",
-							buffer = "[Buffer]",
-							path = "[Path]",
-						})[entry.source.name]
-						return vim_item
-					end,
-				},
-				sources = {
-					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' },
-					{ name = 'buffer' },
-					{ name = 'path' },
-				}
-			})
+        -- ['<Tab>'] = cmp.mapping(function(fallback)
+        -- 	if cmp.visible() then
+        -- 		cmp.select_next_item()
+        -- 	else
+        -- 		fallback()
+        -- 	end
+        -- end, { "i", "s", }),
+        -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+        -- 	if cmp.visible() then
+        -- 		cmp.select_prev_item()
+        -- 	else
+        -- 		fallback()
+        -- 	end
+        -- end, { "i", "s", }),
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              luasnip = "[Snippet]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        }
+      })
 
-			cmp.event:on(
-				'confirm_done',
-				cmp_autopairs.on_confirm_done()
-			)
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
 
-			cmp.setup.filetype({ "sql" }, {
-				sources = {
-					{ name = "vim-dadbod-completion" },
-					{ name = "buffer" },
-				},
-			})
+      cmp.setup.filetype({ "sql" }, {
+        sources = {
+          { name = "vim-dadbod-completion" },
+          { name = "buffer" },
+        },
+      })
 
-			local ls = require "luasnip"
-			ls.config.set_config {
-				history = false,
-				updateevents = "TextChanged,TextChangedI",
-			}
-			for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/custom/snippets/*.lua", true)) do
-				loadfile(ft_path)()
-			end
+      local ls = require "luasnip"
+      ls.config.set_config {
+        history = false,
+        updateevents = "TextChanged,TextChangedI",
+      }
+      for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/custom/snippets/*.lua", true)) do
+        loadfile(ft_path)()
+      end
 
-			vim.keymap.set({ "i", "s" }, "<c-j>", function()
-				if ls.expand_or_jumpable() then
-					ls.expand_or_jump()
-				end
-			end, { silent = true })
+      vim.keymap.set({ "i", "s" }, "<c-j>", function()
+        if ls.expand_or_jumpable() then
+          ls.expand_or_jump()
+        end
+      end, { silent = true })
 
-			vim.keymap.set({ "i", "s" }, "<c-k>", function()
-				if ls.jumpable(-1) then
-					ls.jump(-1)
-				end
-			end, { silent = true })
+      vim.keymap.set({ "i", "s" }, "<c-k>", function()
+        if ls.jumpable(-1) then
+          ls.jump(-1)
+        end
+      end, { silent = true })
 
-			vim.diagnostic.config({
-				virtual_text = false,
-				update_in_insert = true,
-				float = {
-					focusable = false,
-					style = "minimal",
-					border = "rounded",
-					source = "always",
-					header = "",
-					prefix = "",
-				},
-			})
-		end
-	},
+      vim.diagnostic.config({
+        virtual_text = false,
+        update_in_insert = true,
+        float = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
+    end
+  },
 }
