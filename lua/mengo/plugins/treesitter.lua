cb475485b5bdf9586533bc269140878325084826
mengo <menghong6988@gmail.com>
2024-12-07 23:57:04 +0800

    Feat: Added prisma solidity into treesitter

--- lua/mengo/plugins/treesitter.lua
+++ lua/mengo/plugins/treesitter.lua
@@ -10,7 +10,7 @@ return {
     config = function()
       require('nvim-treesitter.configs').setup({
         -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "javascript", "typescript", "bash", "go", "xml", "http", "json", "graphql", "java" },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "javascript", "typescript", "prisma", "solidity", "bash", "go", "xml", "http", "json", "graphql", "java" },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
