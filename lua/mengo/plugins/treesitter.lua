463a5f1cf9f7a844af12939c5554efb99fa5b1d3
mengo <menghong6988@gmail.com>
2024-12-22 22:07:03 +0800

    Feat: removed default keys

--- lua/mengo/plugins/treesitter.lua
+++ lua/mengo/plugins/treesitter.lua
@@ -129,7 +129,6 @@ return {
   },
   {
     'Wansmer/treesj',
-    keys = { '<space>m', '<space>j', '<space>s' },
     dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
     config = function()
       local tsj = require('treesj')
