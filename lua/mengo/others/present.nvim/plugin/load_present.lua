f77d70f9bb4e984f60e989e876b90f8a5c79f890
mengo <menghong6988@gmail.com>
2024-12-31 09:58:24 +0800

    Fix: not importing correctly

--- lua/mengo/others/present.nvim/plugin/load_present.lua
+++ lua/mengo/others/present.nvim/plugin/load_present.lua
@@ -1,3 +1,10 @@
-vim.api.nvim_create_user_command("PresentStart", function ()
+if vim.g.loaded_present == 1 then return end
+vim.g.loaded_present = 1
+
+-- vim.notify("loading present")
+vim.api.nvim_create_user_command("PresentStart", function()
   require("present").start_presentation()
 end, {})
+-- vim.api.nvim_create_user_command("PresentStart", function ()
+--   require("present").start_presentation()
+-- end, {})
--- lua/mengo/plugins/present.lua
+++ lua/mengo/plugins/present.lua
@@ -1,3 +1,9 @@
 return {
-  { dir = "lua/mengo/others/present.nvim"},
+  {
+    dir = vim.fn.stdpath("config") .. "/lua/mengo/others/present.nvim",
+    dev = true,
+    config = function()
+      require("present").setup()
+    end,
+  },
 }
