81309063ed698cf5bcf89dc3a6e2fa5f5ae0451a
mengo <menghong6988@gmail.com>
2025-01-03 13:09:40 +0800

    Feat: In case I want to add mini modules

---
+++ lua/mengo/plugins/mini.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    -- config = function()
+    --   require('mini.surround').setup({
+    --     highlight_duration = 500,
+    --     mappings = {
+    --       add = 'ys',
+    --       delete = 'ds',
+    --       find = '',
+    --       find_left = '',
+    --       highlight = '',
+    --       replace = 'cs',
+    --       update_n_lines = '',
+    --
+    --       -- Add this only if you don't want to use extended mappings
+    --       suffix_last = '',
+    --       suffix_next = '',
+    --     },
+    --     search_method = 'cover_or_next',
+    --   })
+    -- end
+  },
+}
