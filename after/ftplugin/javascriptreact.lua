7959e7cdf52ddffd9734b39a64409a125fdf72f1
mengo <menghong6988@gmail.com>
2025-06-16 06:27:32 +0800

    feat: Add common combos into macros

--- after/ftplugin/javascriptreact.lua
+++ after/ftplugin/javascriptreact.lua
@@ -1,3 +1,12 @@
 local set = vim.opt_local
 set.shiftwidth = 2
 set.tabstop = 2
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*.jsx",
+    callback = function()
+        vim.fn.setreg('r', "diwAMath.floor(Math.random() * pa))")
+        vim.fn.setreg('s', "0f\"s{`;s`}")
+        vim.fn.setreg('c', "vatof>i className=\"\"i")
+    end,
+})
--- after/ftplugin/typescriptreact.lua
+++ after/ftplugin/typescriptreact.lua
@@ -2,3 +2,12 @@ local set = vim.opt_local
 set.shiftwidth = 2
 set.tabstop = 2
 
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*.tsx",
+    callback = function()
+        vim.fn.setreg('r', "diwAMath.floor(Math.random() * pa))")
+        vim.fn.setreg('s', "0f\"cs\"`ysa`{")
+        vim.fn.setreg('c', "vatof>i className=\"\"i")
+    end,
+})
+
