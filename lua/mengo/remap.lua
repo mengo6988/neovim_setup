d0827ba7077c0b3891d39e83a97559ae77197ef1
mengo <menghong6988@gmail.com>
2026-01-27 01:42:30 +0800

    feat: make gd center

--- lua/mengo/remap.lua
+++ lua/mengo/remap.lua
@@ -5,7 +5,6 @@ local term_opts = { silent = true }
 -- Shorten function name
 local keymap = vim.keymap.set
 
-
 -- Remap space as leader key
 keymap("", "( Space )", "<Nop>", opts)
 vim.g.mapleader = " "
@@ -33,29 +32,29 @@ keymap("n", "<leader>np", "<CMD>NoNeckPain<CR>", { desc = "No[N]eck[Pain]" })
 keymap("n", "<leader>nl", "<CMD>Noice last<CR>", { desc = "[N]oice [L]ast" })
 keymap("n", "<leader>nd", "<CMD>Noice dismiss<CR>", { desc = "[N]oice [D]ismiss" })
 keymap("n", "<leader>nh", "<CMD>Noice history<CR>", { desc = "[N]oice [H]istory" })
-keymap("n", "<leader>on", "<CMD>Nvumi<CR>", {desc = "[O]pen [N]vumi"})
+keymap("n", "<leader>on", "<CMD>Nvumi<CR>", { desc = "[O]pen [N]vumi" })
 
-keymap('n', '<leader>sr', ':source %<CR>')
+keymap("n", "<leader>sr", ":source %<CR>")
 
 -- Terminal related
-keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-keymap('t', '< C-d >', '<C-\\><C-n><cmd>bd!<CR>', { desc = 'Exit terminal mode' })
-keymap('n', '<leader>st', function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 17)
+keymap("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+keymap("t", "< C-d >", "<C-\\><C-n><cmd>bd!<CR>", { desc = "Exit terminal mode" })
+keymap("n", "<leader>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 17)
 
-  job_id = vim.bo.channel
+	job_id = vim.bo.channel
 end)
 -- Testing here, Idk what to put yet, but maybe can have a complicated one to use, suggestion (send highlighted command into terminal)
-keymap('n', "<leader>example", function()
-  vim.fn.chansend(job_id, { 'echo "hello"\r\n' })
+keymap("n", "<leader>example", function()
+	vim.fn.chansend(job_id, { 'echo "hello"\r\n' })
 end)
 
 -- Save quit etc
 keymap("n", "<leader>w", ":w!<CR>", opts)
-keymap('n', '<leader>wf', '<cmd>FormatEnable<CR><cmd>w<cr><cmd>FormatDisable<CR>', opts)
+keymap("n", "<leader>wf", "<cmd>FormatEnable<CR><cmd>w<cr><cmd>FormatDisable<CR>", opts)
 keymap("n", "<leader>x", ":x!<CR>", opts)
 keymap("n", "<leader>q", ":q!<CR>", opts)
 keymap("n", "<leader>wq", ":wq!<CR>", opts)
@@ -76,14 +75,14 @@ keymap("n", "<leader>,", ":only<CR>", { noremap = true, silent = true, desc = "F
 keymap("n", "<leader>c", ":close<CR>", { noremap = true, silent = true, desc = "Close" })
 
 -- Special Remaps
-keymap('n', 'gV', '`[v`]', { noremap = true, desc = 'Select last changed or yanked text' })
-keymap('n', 'yc', 'yy<cmd>normal gcc<CR>p', { desc = "Copy paste and comment the line copied" })
-keymap('n', '<C-s><C-s>', ':.!sh<cr>', { noremap = true, desc = 'Send current line to sh and REPLACE with the output' })
+keymap("n", "gV", "`[v`]", { noremap = true, desc = "Select last changed or yanked text" })
+keymap("n", "yc", "yy<cmd>normal gcc<CR>p", { desc = "Copy paste and comment the line copied" })
+keymap("n", "<C-s><C-s>", ":.!sh<cr>", { noremap = true, desc = "Send current line to sh and REPLACE with the output" })
 keymap("i", "jk", "<Esc>", opts)
 
 -- Better navigation
-keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Better File Navigation
 keymap("n", "<C-d>", "<C-d>zz", opts)
@@ -95,7 +94,6 @@ keymap("n", "N", "Nzz", opts)
 keymap("n", "*", "*zz", opts)
 keymap("n", "#", "#zz", opts)
 keymap("n", "G", "Gzz", opts)
-
 
 -- Resize with arrows
 keymap("n", "<C-S-Up>", ":resize +2<CR>", opts)
@@ -115,13 +113,12 @@ keymap("n", "˚", ":m .-2<CR>==", opts)
 -- Stay in indent mode
 keymap("v", "<", "<gv^", opts)
 keymap("v", ">", ">gv^", opts)
-keymap("v", "<leader>y", "\"+y", opts)
+keymap("v", "<leader>y", '"+y', opts)
 
 -- Move text up and down
 keymap("v", "˚", ":m '<-2<CR>gv=gv", opts)
 keymap("v", "∆", ":m '>+1<CR>gv=gv", opts)
 keymap("v", "p", '"_dP', opts)
-
 
 -- Visual Block --
 -- Move text up and down
@@ -130,66 +127,71 @@ keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
 keymap("x", "∆", ":m '>+1<CR>gv=gv", opts)
 keymap("x", "˚", ":m '<-2<CR>gv=gv", opts)
 
-
 -- LSP attach for lsp commands
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true }),
-  callback = function(event)
-    local map = function(keys, func, desc)
-      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-    end
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+	callback = function(event)
+		local map = function(keys, func, desc)
+			vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+		end
 
-    -- Jump to the definition of the word under your cursor.
-    --  This is where a variable was first declared, or where a function is defined, etc.
-    --  To jump back, press <C-t>.
-    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+		-- Jump to the definition of the word under your cursor.
+		--  This is where a variable was first declared, or where a function is defined, etc.
+		--  To jump back, press <C-t>.
+		map("gd", function()
+			require("telescope.builtin").lsp_definitions()
+			vim.schedule(function()
+				vim.cmd("normal! zz")
+			end)
+		end, "[G]oto [D]efinition")
+		-- Find references for the word under your cursor.
+		map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
-    -- Find references for the word under your cursor.
-    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+		-- Jump to the implementation of the word under your cursor.
+		--  Useful when your language has ways of declaring types without an actual implementation.
+		map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
-    -- Jump to the implementation of the word under your cursor.
-    --  Useful when your language has ways of declaring types without an actual implementation.
-    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+		-- Jump to the type of the word under your cursor.
+		--  Useful when you're not sure what type a variable is and you want to see
+		--  the definition of its *type*, not where it was *defined*.
+		map("<leader>vD", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
-    -- Jump to the type of the word under your cursor.
-    --  Useful when you're not sure what type a variable is and you want to see
-    --  the definition of its *type*, not where it was *defined*.
-    map('<leader>vD', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+		-- Fuzzy find all the symbols in your current document.
+		--  Symbols are things like variables, functions, types, etc.
+		map("<leader>vds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
-    -- Fuzzy find all the symbols in your current document.
-    --  Symbols are things like variables, functions, types, etc.
-    map('<leader>vds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+		-- Fuzzy find all the symbols in your current workspace.
+		--  Similar to document symbols, except searches over your entire project.
+		map("<leader>vws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+		map("<leader>vd", function()
+			vim.diagnostic.open_float()
+		end, "[D]iagnostic Float")
 
-    -- Fuzzy find all the symbols in your current workspace.
-    --  Similar to document symbols, except searches over your entire project.
-    map('<leader>vws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-    map('<leader>vd', function() vim.diagnostic.open_float() end, '[D]iagnostic Float')
+		-- Rename the variable under your cursor.
+		--  Most Language Servers support renaming across files, etc.
+		map("<leader>vrn", vim.lsp.buf.rename, "[R]e[n]ame")
+		map("<leader>vh", vim.lsp.buf.signature_help, "Signature [Help]")
 
-    -- Rename the variable under your cursor.
-    --  Most Language Servers support renaming across files, etc.
-    map('<leader>vrn', vim.lsp.buf.rename, '[R]e[n]ame')
-    map('<leader>vh', vim.lsp.buf.signature_help, 'Signature [Help]')
+		-- Execute a code action, usually your cursor needs to be on top of an error
+		-- or a suggestion from your LSP for this to activate.
+		map("<leader>vca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-    -- Execute a code action, usually your cursor needs to be on top of an error
-    -- or a suggestion from your LSP for this to activate.
-    map('<leader>vca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+		-- Opens a popup that displays documentation about the word under your cursor
+		--  See `:help K` for why this keymap.
+		map("K", vim.lsp.buf.hover, "Hover Documentation")
 
-    -- Opens a popup that displays documentation about the word under your cursor
-    --  See `:help K` for why this keymap.
-    map('K', vim.lsp.buf.hover, 'Hover Documentation')
+		-- WARN: This is not Goto Definition, this is Goto Declaration.
+		--  For example, in C this would take you to the header.
+		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-    -- WARN: This is not Goto Definition, this is Goto Declaration.
-    --  For example, in C this would take you to the header.
-    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-    map('<leader>vwa', vim.lsp.buf.add_workspace_folder, "add workspace folder")
-    map('<leader>vwr', vim.lsp.buf.remove_workspace_folder, "rm workspace folder")
-    map('<leader>vwl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace Folder [L]ist')
-    map('<leader>vrn', vim.lsp.buf.rename, 'Buffer [R]e[N]ame')
-  end
+		map("<leader>vwa", vim.lsp.buf.add_workspace_folder, "add workspace folder")
+		map("<leader>vwr", vim.lsp.buf.remove_workspace_folder, "rm workspace folder")
+		map("<leader>vwl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, "[W]orkspace Folder [L]ist")
+		map("<leader>vrn", vim.lsp.buf.rename, "Buffer [R]e[N]ame")
+	end,
 })
 
 -- Refactor nvim remaps
@@ -217,4 +219,8 @@ vim.keymap.set("n", "<leader>gdl", ":diffget //3<CR>")
 
 vim.keymap.set("n", "<leader>sc", "<cmd>Scratch<cr>")
 vim.keymap.set("n", "<leader>sco", "<cmd>ScratchOpen<cr>")
-vim.keymap.set("n", "<leader>srf", ":lua require('grug-far').open({ prefills = { paths = vim.fn.expand('%'), transient=true, engine='astgrep' } })<CR>")
+vim.keymap.set(
+	"n",
+	"<leader>srf",
+	":lua require('grug-far').open({ prefills = { paths = vim.fn.expand('%'), transient=true, engine='astgrep' } })<CR>"
+)
