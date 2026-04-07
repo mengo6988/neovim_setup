d8410600f4cac749caed53e4e9b7345a37adf2a1
mengo <menghong6988@gmail.com>
2026-04-07 22:12:33 +0800

    feat: update lazy git to use snack's

--- lua/mengo/plugins/init.lua
+++ lua/mengo/plugins/init.lua
@@ -1,5 +1,26 @@
 return {
-	{ "folke/snacks.nvim", lazy = false, priority = 1000, opts = {} },
+	{
+    "folke/snacks.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      lazygit = { enabled = true },
+      -- indent = {
+      --   enabled = true,
+      --   animate = false,
+      -- },
+    },
+    keys = {
+    { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+    { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+    { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+    { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+    { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+    { "<leader>lg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    }
+  },
 	{
 		"nvim-lua/plenary.nvim",
 		name = "plenary",
