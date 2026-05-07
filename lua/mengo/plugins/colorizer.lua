287e8f5af23cf30c8f8f1adbb2fc3be07b60e155
mengo <menghong6988@gmail.com>
2026-05-08 01:15:22 +0800

    chore: plugin updates + fix colorizer

--- lua/mengo/plugins/colorizer.lua
+++ lua/mengo/plugins/colorizer.lua
@@ -1,11 +1,26 @@
 return {
 	"catgoose/nvim-colorizer.lua",
-	ft = { "css", "javascript", "html", "scss", "typescriptreact", "javascriptreact" },
+	ft = { "css", "scss", "html", "javascript", "javascriptreact", "typescript", "typescriptreact" },
 	config = function()
 		require("colorizer").setup({
-			filetypes = { "css", "javascript", "html" },
-			user_default_options = {
-				mode = "background",
+			filetypes = {
+				"css",
+				"scss",
+				"html",
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+			},
+			options = {
+				parsers = {
+					css = true,
+					tailwind = { enable = true, lsp = true },
+				},
+				display = {
+					mode = "foreground",
+					disable_document_color = true,
+				},
 			},
 		})
 	end,
